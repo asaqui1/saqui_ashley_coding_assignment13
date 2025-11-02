994236261bde57c47595e17c961e932e7e693bca
asaqui1 <asaqui@academic.rrc.ca>
2025-11-01 19:45:43 -0500

    Fix ESLint config completely

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
 
 export default [
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
