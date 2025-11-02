999cc6f549a14049aa2a48f7910f76566307155b
asaqui1 <asaqui@academic.rrc.ca>
2025-11-01 19:32:52 -0500

    Fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,8 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
+export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: { js },
@@ -13,4 +12,4 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+];
